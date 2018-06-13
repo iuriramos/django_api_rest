54a92e65ecd3826ec25ca4278fda2dcee13e3fbb
Ruan <ruannawe95@gmail.com>
2018-06-13 18:28:21 -0300

    [ REMOVE ] Removendo classe desnecessária

--- projects/services.py
+++ projects/services.py
@@ -52,17 +52,3 @@ class ProjectService():
         instance = model.objects.get(pk=id)
         return instance.delete()
 
-
-class ResponseService():
-    
-    def __init__(self, data, success):
-        self.__data__ = data
-        self.__success__ = success
-
-
-    def get_data(self):
-        return self.__data__
-
-
-    def get_success(self):
-        return self.__success__
