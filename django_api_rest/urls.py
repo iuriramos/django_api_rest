03846e74d88e8043967735ec1dbefc60879114db
iuriramos <iuri.srb@gmail.com>
2018-06-13 22:33:57 -0300

    Standardize url patterns

--- django_api_rest/urls.py
+++ django_api_rest/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('docs/', include_docs_urls(title='API REST Documents', description='RESTful API Docuementation')),
-    path('projects/', include('projects.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^projects/', include('projects.urls')),
+    url(r'^docs/', include_docs_urls(title='API REST Documents', description='RESTful API Docuementation')),
 ]
--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,11 @@
-from django.urls                  import path
+from django.conf.urls             import url
 from rest_framework.urlpatterns   import format_suffix_patterns
 from .views                       import ProjectView
 
 urlpatterns = [
-    path('all', ProjectView.all),
-    path('get/<int:id>', ProjectView.get),
-    path('create', ProjectView.create),
-    path('update/<int:id>', ProjectView.update),
-    path('destroy/<int:id>', ProjectView.destroy),
+    url(r'^all/', ProjectView.all),
+    url(r'^get/(?P<id>\d+)/', ProjectView.get),
+    url(r'^create/', ProjectView.create),
+    url(r'^update/(?P<id>\d+)/', ProjectView.update),
+    url(r'^destroy/(?P<id>\d+)/', ProjectView.destroy),
 ]
